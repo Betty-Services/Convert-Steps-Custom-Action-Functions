bcdce8ff747ad64f43b6205764d2277933e4a064
Maarten Geerse <mgeerse@gmail.com>
2023-03-13 13:18:52 -0400

    feat: back to 1.0 for convert json to obj

--- functions/convert-json-to-object/1.0/index.js
+++ functions/convert-json-to-object/1.0/index.js
@@ -1,11 +1,7 @@
 const convertJsonToObject = async ({ input }) => {
-  try {
-    return {
-      as: JSON.parse(`${input}`),
-    };
-  } catch (e) {
-    throw new Error(e);
-  }
+  return {
+    as: JSON.parse(`${input}`),
+  };
 };
 
 export default convertJsonToObject;
